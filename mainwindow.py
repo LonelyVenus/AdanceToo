c2a0cde7145b22357c56610cd2cb8c416e704905
mys <1178703620@qq.com>
2020-08-11 14:02:56 +0800

    1、增加了USB和TCP窗口 2、增加串口打开、数据发送和接收功能 3、其它功能型界面调整

--- mainwindow.py
+++ mainwindow.py
@@ -5,6 +5,8 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QGraphicsScene, QGraphics
 from PyQt5.QtGui import QImage, QPixmap, QIcon
 from topwindow import Ui_TopWindow
 from serialwindow import SerialWindow
+from tcpudpwindow import TcpUdpWindow
+from usbwindow import UsbWindow
 
 
 class MainWindow(QtWidgets.QMainWindow,Ui_TopWindow):
@@ -18,10 +20,62 @@ class MainWindow(QtWidgets.QMainWindow,Ui_TopWindow):
         self.statusbar.setStyleSheet("background-color: rgb(104, 33, 122);")
 
         self.serial_ui = SerialWindow()
+        self.tcpudp_ui = TcpUdpWindow()
+        self.usb_ui = UsbWindow()
 
         self.stackWidget = QtWidgets.QStackedWidget()
         self.gridLayout.addWidget(self.stackWidget)
         self.stackWidget.addWidget(self.serial_ui)
+        self.stackWidget.addWidget(self.tcpudp_ui)
+        self.stackWidget.addWidget(self.usb_ui)
+
+        self.SerialUI()
+
+        self.SerialpushButton.clicked.connect(self.SerialUI)
+        self.TCPpushButton.clicked.connect(self.TCPUI)
+        self.USBpushButton.clicked.connect(self.USBUI)
+
+    def SerialUI(self):
+        self.stackWidget.setCurrentIndex(0)
+        self.SerialpushButton.setStyleSheet("font: 22pt \"华文中宋\";\n"
+                                         "color: rgb(255, 255, 255);\n"
+                                         "background-color: rgb(64, 65, 66);\n"
+                                         "")
+        self.TCPpushButton.setStyleSheet("font: 22pt \"华文中宋\";\n"
+                                         "color: rgb(0, 0, 0);\n"
+                                         "")
+        self.USBpushButton.setStyleSheet("font: 22pt \"华文中宋\";\n"
+                                            "color: rgb(0, 0, 0);\n"
+                                            "")
+
+    def TCPUI(self):
+        self.stackWidget.setCurrentIndex(1)
+        self.TCPpushButton.setStyleSheet("font: 22pt \"华文中宋\";\n"
+                                         "color: rgb(255, 255, 255);\n"
+                                         "background-color: rgb(64, 65, 66);\n"
+                                         "")
+        self.USBpushButton.setStyleSheet("font: 22pt \"华文中宋\";\n"
+                                         "color: rgb(0, 0, 0);\n"
+                                         "")
+        self.SerialpushButton.setStyleSheet("font: 22pt \"华文中宋\";\n"
+                                            "color: rgb(0, 0, 0);\n"
+                                            "")
+
+    def USBUI(self):
+        self.stackWidget.setCurrentIndex(2)
+        self.USBpushButton.setStyleSheet("font: 22pt \"华文中宋\";\n"
+                                         "color: rgb(255, 255, 255);\n"
+                                         "background-color: rgb(64, 65, 66);\n"
+                                         "")
+        self.TCPpushButton.setStyleSheet("font: 22pt \"华文中宋\";\n"
+                                         "color: rgb(0, 0, 0);\n"
+                                         "")
+        self.SerialpushButton.setStyleSheet("font: 22pt \"华文中宋\";\n"
+                                            "color: rgb(0, 0, 0);\n"
+                                            "")
+
+
+
 
         # self.icon_image = QImage()
         # self.icon_image.load(".\logo_min.jpg")
--- serialwindow.py
+++ serialwindow.py
@@ -1,7 +1,9 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import QTimer,QSize,QEvent,Qt
 from serial_ui import Ui_serial_ui
-from PyQt5.QtWidgets import QMessageBox, QInputDialog
+from PyQt5.QtWidgets import QMessageBox, QInputDialog,QDialog
+from PyQt5.QtGui import QImage, QPixmap, QMouseEvent
+import time
 
 import serial
 import serial.tools.list_ports
@@ -18,19 +20,40 @@ class SerialWindow(QtWidgets.QWidget,Ui_serial_ui):
         self.Com_Dict = {}
         self.port_list = list()
         self.PortCheck()
-        self.scanTimer = QTimer()
-        self.scanTimer.timeout.connect(self.PortCheck)
-        self.scanTimer.start(2000)
+        self.comIDcomboBox.installEventFilter(self)
 
         #串口声明
         self.ser = serial.Serial()
 
+        self.icon1 = QtGui.QIcon()
+        self.icon1.addPixmap(QtGui.QPixmap("off_1.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
 
+        self.icon2 = QtGui.QIcon()
+        self.icon2.addPixmap(QtGui.QPixmap("on_1.png"), QtGui.QIcon.Normal, QtGui.QIcon.On)
+        self.comOpenpushButton.setIcon(self.icon1)
+        self.comOpenpushButton.setIconSize(QtCore.QSize(32, 32))
+
+        # 定时器接收数据
+        self.recv_timer = QTimer(self)
+        self.recv_timer.timeout.connect(self.SerialRecv)
+
+
+    def eventFilter(self, obj, event):
+        if obj == self.comIDcomboBox:
+            if event.type() == QEvent.MouseButtonPress:
+                mouseEvent = QMouseEvent(event)
+                if mouseEvent.buttons() == Qt.LeftButton:
+                    self.PortCheck()
+        return QtWidgets.QWidget.eventFilter(self, obj, event)
 
     def InitSignalConnect(self):
         self.comExpandpushButton.clicked.connect(self.ExpandToolCtl)
         self.comOpenpushButton.clicked.connect(self.PortOpenAndClose)
         self.comBaudcomboBox.currentIndexChanged.connect(self.BaudrateCustom)
+        self.comSendpushButton.clicked.connect(self.SerialSend)
+        self.comOpenFilepushButton.clicked.connect(self.OpenSendFile)
+
+
 
 
     def BaudrateCustom(self):
@@ -83,6 +106,10 @@ class SerialWindow(QtWidgets.QWidget,Ui_serial_ui):
             return None
         if self.comOpenpushButton.text() == "打开串口" :
             self.comOpenpushButton.setText("关闭串口")
+            self.comOpenpushButton.setStyleSheet("font: 15pt \"黑体\";\n"
+                                                 "color: rgb(0, 0, 0);\n"
+                                                 "background-color: rgb(248, 177, 1);")
+            self.comOpenpushButton.setIcon(self.icon2)
             self.ser.port = self.comIDcomboBox.currentText().split(' ',1)[0]
             self.ser.baudrate = int(self.comBaudcomboBox.currentText())
             self.ser.bytesize = int(self.comBitlencomboBox.currentText())
@@ -90,13 +117,101 @@ class SerialWindow(QtWidgets.QWidget,Ui_serial_ui):
 
             try:
                 self.ser.open()
+                recv_timeout = int(self.comRecvTimeoutEdit.text())
+                self.recv_timer.start(recv_timeout)
             except:
+                self.comOpenpushButton.setText("打开串口")
+                self.recv_timer.stop()
+                self.comOpenpushButton.setStyleSheet("font: 15pt \"黑体\";\n"
+                                                     "color: rgb(0, 0, 0);\n"
+                                                     "background-color: rgb(55, 54, 52);")
+                self.comOpenpushButton.setIcon(self.icon1)
                 QMessageBox.critical(self, "Port Error", "此串口不能被打开！")
                 return None
 
         else:
             self.comOpenpushButton.setText("打开串口")
+            self.comOpenpushButton.setStyleSheet("font: 15pt \"黑体\";\n"
+                                                 "color: rgb(0, 0, 0);\n"
+                                                 "background-color: rgb(55, 54, 52);")
+            self.comOpenpushButton.setIcon(self.icon1)
+            self.recv_timer.stop()
             try:
                 self.ser.close()
             except:
                 return None
+
+    def SerialSend(self):
+        if self.ser.isOpen():
+            input_s = self.ComSendtextEdit.toPlainText()
+            if input_s != "":
+                # 非空字符串
+                if self.comSendHexcheckBox.isChecked():
+                    # hex发送
+                    input_s = input_s.strip()
+                    send_list = []
+                    while input_s != '':
+                        try:
+                            num = int(input_s[0:2], 16)
+                        except ValueError:
+                            QMessageBox.critical(self, 'wrong data', '请输入十六进制数据，以空格分开!')
+                            return None
+                        input_s = input_s[2:].strip()
+                        send_list.append(num)
+                    input_s = bytes(send_list)
+                    display_str = '[hex]'+''.join(['%02X ' % b for b in input_s])
+                else:
+                    # ascii发送
+                    input_s = input_s.encode('utf-8')
+                    display_str = str(input_s, encoding = "utf-8")
+                try:
+                    num = self.ser.write(input_s)
+                except:
+                    return
+                cur_time = str(time.strftime('%H:%M:%S',time.localtime(time.time()))+'.'+str(int((time.time()*1000)%1000)))
+                self.ComRecvtextBrowser.append('['+cur_time+']'+'发'+'->'+' '+ display_str )
+
+        else:
+            pass
+
+        # 接收数据
+    def SerialRecv(self):
+        try:
+            num = self.ser.inWaiting()
+        except:
+            self.recv_timer.stop()
+            self.comOpenpushButton.setText("打开串口")
+            self.comOpenpushButton.setStyleSheet("font: 15pt \"黑体\";\n"
+                                                 "color: rgb(0, 0, 0);\n"
+                                                 "background-color: rgb(55, 54, 52);")
+            self.comOpenpushButton.setIcon(self.icon1)
+            try:
+                self.ser.close()
+            except:
+                return None
+            return None
+        if num > 0:
+            data = self.ser.read(num)
+            num = len(data)
+            # hex显示
+            cur_time = str(time.strftime('%H:%M:%S', time.localtime(time.time())) + '.' + str(
+                int((time.time() * 1000) % 1000)))
+            if self.comRecvHexcheckBox.checkState():
+                out_s = ''
+                for i in range(0, len(data)):
+                    out_s = out_s + '{:02X}'.format(data[i]) + ' '
+                self.ComRecvtextBrowser.append('[' + cur_time + ']' + '收' + '->' + ' ' + '[hex]'+out_s)
+                #self.ComRecvtextBrowser.insertPlainText(out_s)
+            else:
+                # 串口接收到的字符串为b'123',要转化成unicode字符串才能输出到窗口中去
+                self.ComRecvtextBrowser.append('[' + cur_time + ']' + '收' + '->' + ' ' + data.decode('iso-8859-1'))
+        else:
+            pass
+
+    def OpenSendFile(self):
+        # download_path = QtWidgets.QFileDialog.getExistingDirectory(self,
+        #                                                            "打开",
+        #                                                            "./")
+        download_path = QtWidgets.QFileDialog.getOpenFileName(self,'打开文件','./')
+        self.comSendFilePathlineEdit.setText(download_path[0])
+
